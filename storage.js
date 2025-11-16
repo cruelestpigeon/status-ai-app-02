b98531b0e7bce70bf130f409440bed88ec241db6
cruelestpigeon <sketchbooktown2@gmail.com>
2025-11-15 22:21:54 -0500

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-
-function save(key, value){
-  localStorage.setItem(key, JSON.stringify(value));
-}
-function load(key, fallback){
-  const v = localStorage.getItem(key);
-  return v ? JSON.parse(v) : fallback;
-}
